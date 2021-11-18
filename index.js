f5718f2ded04cc14d18a7084f967720daf3fb6e9
Euller Braz <euller.braz99@gmail.com>
2021-11-18 15:47:08 -0300

    Primeiro commit, iniciada instancia do express

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
